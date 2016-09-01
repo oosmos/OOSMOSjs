d2b5a6ac7f58b25468125ed8c96d045645cae8cb
markjglenn <mark.glenn@oosmos.com>
2016-09-01 15:09:43 -0700

    ts/js split

--- ts/build.py
+++ ts/build.py
@@ -6,10 +6,10 @@ import shutil
 os.system("tsc")
 os.system('tsc -d --outDir tmp OOSMOS.ts')
 
-if os.path.exists('typings/OOSMOS.d.ts'):
-  os.remove('typings/OOSMOS.d.ts')
+if os.path.exists('OOSMOS.d.ts'):
+  os.remove('OOSMOS.d.ts')
 
-shutil.move('tmp/OOSMOS.d.ts', 'typings/OOSMOS.d.ts')
+shutil.move('tmp/OOSMOS.d.ts', 'OOSMOS.d.ts')
 shutil.rmtree('tmp')
 
 os.system("webpack")
